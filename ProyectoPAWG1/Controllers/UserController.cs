3b7136ac04318ced9adb88af58d9d021e46bdf99
KennethUN <knavarro60492@gmail.com>
2024-11-15 18:13:15 -0600

    Cambio en el create y edit de users admin

--- ProyectoPAWG1/Controllers/UserController.cs
+++ ProyectoPAWG1/Controllers/UserController.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.Options;
 using PAWG1.Mvc.Models;
 using PAWG1.Architecture.Exceptions;
 using PAWG1.Models.EFModels;
+using System.Text.Json;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ProyectoPAWG1.Controllers
 {
@@ -48,6 +52,26 @@ namespace ProyectoPAWG1.Controllers
         public async Task<IActionResult> Create([Bind("Username,Email,Password,State,IdRole")] CMP.User user)
         {
             ModelState.Remove("IdRoleNavigation");
+            var data = await _restProvider.GetAsync($"{_appSettings.Value.RestApi}/UserApi/all", null);
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var existingUsers = JsonSerializer.Deserialize<List<CMP.User>>(data, options);
+
+            // Validación del Username
+            if (existingUsers != null && existingUsers.Any(existingUser => existingUser.Username == user.Username))
+            {
+                ModelState.AddModelError("Username", "The username already exists. Please choose another one.");
+
+                var dataRoles = await _restProvider.GetAsync($"{_appSettings.Value.RestApi}/RoleApi/all", null);
+
+                var roles = JsonProvider.DeserializeSimple<IEnumerable<CMP.Role>>(dataRoles);
+
+                ViewBag.Roles = roles;
+                return View(user);
+            }
+
+            // Encriptar la contraseña
+            user.Password = HashPassword(user.Password);
 
             //ModelState.IdRolenNavigation = ModelState.IdRole;
             if (ModelState.IsValid)
@@ -70,6 +94,12 @@ namespace ProyectoPAWG1.Controllers
                 return NotFound();
 
             var user = await _restProvider.GetAsync($"{_appSettings.Value.RestApi}/UserApi/{id}", $"{id}");
+
+            var data = await _restProvider.GetAsync($"{_appSettings.Value.RestApi}/RoleApi/all", null);
+
+            var roles = JsonProvider.DeserializeSimple<IEnumerable<CMP.Role>>(data);
+
+            ViewBag.Roles = roles;
             if (user == null)
                 return NotFound();
 
@@ -86,6 +116,39 @@ namespace ProyectoPAWG1.Controllers
             if (user == null || id != user.IdUser)
                 return NotFound();
 
+            var dataUserId = await _restProvider.GetAsync($"{_appSettings.Value.RestApi}/UserApi/{id}", $"{id}");
+            var userId = JsonProvider.DeserializeSimple<User>(dataUserId);
+
+            if(userId.Username != user.Username)
+            {
+                var dataUsers = await _restProvider.GetAsync($"{_appSettings.Value.RestApi}/UserApi/all", null);
+
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var existingUsers = JsonSerializer.Deserialize<List<CMP.User>>(dataUsers, options);
+
+                // Validación del Username
+                if (existingUsers != null && existingUsers.Any(existingUser => existingUser.Username == user.Username))
+                {
+                    ModelState.AddModelError("Username", "The username already exists. Please choose another one.");
+
+                    var dataRoles = await _restProvider.GetAsync($"{_appSettings.Value.RestApi}/RoleApi/all", null);
+
+                    var roles = JsonProvider.DeserializeSimple<IEnumerable<CMP.Role>>(dataRoles);
+
+                    ViewBag.Roles = roles;
+                    return View(user);
+                }
+            }
+
+            if (user.Password == null)
+            {
+                var userg = await _restProvider.GetAsync($"{_appSettings.Value.RestApi}/UserApi/{id}", $"{id}");
+                var getuser = JsonProvider.DeserializeSimple<User>(userg);
+
+                user.Password = getuser.Password;
+                ModelState.Remove("Password");
+            }
+            user.Password = HashPassword(user.Password);
             User? updated = default;
             if (ModelState.IsValid)
             {
@@ -98,6 +161,24 @@ namespace ProyectoPAWG1.Controllers
             }
             return View(updated);
         }
+
+        private static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (var b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+
+
+
 
     }
 }
