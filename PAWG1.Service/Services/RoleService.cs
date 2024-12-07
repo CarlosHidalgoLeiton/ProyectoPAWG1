c3a698af89e0195ffd29c39142f5b7852375802f
jimenaaJimenez <jjimenez80420@ufide.ac.cr>
2024-12-07 14:24:33 -0600

    comentarios services

--- PAWG1.Service/Services/RoleService.cs
+++ PAWG1.Service/Services/RoleService.cs
@@ -1,39 +1,58 @@
-﻿using PAWG1.Models;
-using PAWG1.Data.Repository;
-using CMP = PAWG1.Models.EFModels;
+﻿using PAWG1.Data.Repository;
 using PAWG1.Models.EFModels;
-
+using CMP = PAWG1.Models.EFModels;
 namespace PAWG1.Service.Services;
 
+/// <summary>
+/// Interface for role-related services.
+/// </summary>
 public interface IRoleService
 {
+    /// <summary>
+    /// Asynchronously retrieves all roles.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation, containing all <see cref="Role"/>.</returns>
     Task<IEnumerable<Role>> GetAllRolesAsync();
+
+    /// <summary>
+    /// Asynchronously retrieves a role by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the role to retrieve.</param>
+    /// <returns>A task that represents the asynchronous operation, containing the requested <see cref="Role"/>.</returns>
     Task<CMP.Role> GetRoleAsync(int id);
 }
 
+
+/// <summary>
+/// Implementation of the <see cref="IRoleService"/> interface.
+/// </summary>
 public class RoleService : IRoleService
 {
     private readonly IRoleRepository _roleRepository;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="CategoryService"/> class.
+    /// Initializes a new instance of the <see cref="RoleService"/> class.
     /// </summary>
-    /// <param name="CategoryRepository">The repository used to access Category data.</param>
+    /// <param name="roleRepository">The repository used to access Role data.</param>
     public RoleService(IRoleRepository roleRepository)
     {
         _roleRepository = roleRepository;
     }
 
     /// <summary>
-    /// Asynchronously retrieves all Categorys.
+    /// Asynchronously retrieves all Roles.
     /// </summary>
-    /// <returns>A task that represents the asynchronous operation, containing all <see cref="Category"/>.</returns>
+    /// <returns>A task that represents the asynchronous operation, containing all <see cref="Role"/>.</returns>
     public async Task<IEnumerable<CMP.Role>> GetAllRolesAsync()
     {
         return await _roleRepository.GetAllRolesAsync();
     }
 
-
+    /// <summary>
+    /// Asynchronously retrieves a role by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the role to retrieve.</param>
+    /// <returns>A task that represents the asynchronous operation, containing the requested <see cref="Role"/>.</returns>
     public async Task<CMP.Role> GetRoleAsync(int id)
     {
         return await _roleRepository.GetRoleAsync(id);
